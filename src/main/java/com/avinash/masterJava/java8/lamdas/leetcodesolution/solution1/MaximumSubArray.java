5c4139d3eccfdf9696b25393fd946ea9da1a19e6
avinash <avinashmdevadig@gmail.com>
2026-08-21 10:16:05 +0530

    feat: leet code solution #1

--- src/main/java/com/avinash/masterJava/java8/lamdas/leetcodesolution/solution1/MaximumSubArray.java
+++ src/main/java/com/avinash/masterJava/java8/lamdas/leetcodesolution/solution1/MaximumSubArray.java
@@ -26,7 +26,7 @@ Output: 23
 Explanation: The subarray [5,4,-1,7,8] has the largest sum = 23.
 * */
 public class MaximumSubArray {
-
+//my solution not upto mark :(
     private static void maxSubArray(int[] arry) {
         if (arry.length == 0) {
             System.out.println("Array Is Empty!!!");
@@ -55,8 +55,39 @@ public class MaximumSubArray {
         System.out.println(answer);
         System.out.println(intList);
     }
+
+    private static void printMaxSunSubArray(int[] inputArray){
+        int maxSoFar = inputArray[0];
+        int currentMax = inputArray[0];
+        int start = 0,end = 0,temp = 0;
+
+        for (int i = 1; i < inputArray.length; i++) {
+            if(currentMax+inputArray[i]< inputArray[i]){
+                currentMax = inputArray[i];
+                temp = i;
+            }else{
+                currentMax += inputArray[i];
+            }
+
+            if(currentMax > maxSoFar){
+                maxSoFar = currentMax;
+                start = temp;
+                end =i;
+            }
+
+        }
+
+        System.out.println("max sub array :: "+ Arrays.toString(Arrays.copyOfRange(inputArray, start, end)));
+        System.out.println(" max sum of sub array"+ maxSoFar);
+
+    }
     public static void main(String[] args) {
         int[] inputs = {-2,1,-3,4,-1,2,1,-5,4};
-        maxSubArray(inputs);
+        int[] inputs1 = {1,1,2,-2,1,3};
+        int[] inputs2 = {5,4,-1,7,8};
+        maxSubArray(inputs);// not recomended
+        printMaxSunSubArray(inputs); // see this solution
+        printMaxSunSubArray(inputs1);
+        printMaxSunSubArray(inputs2);
     }
 }
